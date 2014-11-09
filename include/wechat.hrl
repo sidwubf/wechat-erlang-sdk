02e8414bdf06c7a055d0b2cc1c8c056e3b8fdd6f
SidWu <wubofengwbf@gmail.com>
2014-11-09 16:30:36 +0800

    send text message

---
+++ include/wechat.hrl
@@ -0,0 +1,4 @@
+-ifndef(WECHAT_HRL).
+-define(WECHAT_HRL, "wechat.hrl").
+
+-define(WECHAT_API_URI, "api.weixin.qq.com").
--- src/wechat.erl
+++ src/wechat.erl
@@ -1,9 +1,21 @@
 -module(wechat).
 
--export([test/0]).
+-export([send_text_message/3]).
 
-test() ->
-    Access_Token = "fjoajfoawjfoawjofjwaofj",
-    {ok, Pid} = gun:open("api.weixin.qq.com", 443),
-    StreamRef = gun:post(Pid, "/cgi-bin/message/custom/send?access_token="++Access_Token, [], []),
+-include("include/wechat.hrl").
+
+-send_text_message(AccessToken, OpenID, Content) ->
+    {ok, Pid} = gun:open(?WECHAT_API_URI, 443),
+    Path = "/cgi-bin/message/custom/send?access_token=" ++ AccessToken,
+    Headers = [
+               {"accept", "*/*"},
+               {"user-agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36"},
+               {"referer", "https://mp.weixin.qq.com/"},
+               {"accept-encoding", "gzip,deflate"},
+               {"accept-language", "zh-CN,zh;q=0.8"},
+               {"content-type", "application/x-www-form-urlencoded; charset=UTF-8"}
+              ],
+    Body = "{\"touser\":\""++OpenID++"\",\"msgtype\":\"text\",\"text\":{\"content\":\""++Content++"\"}}",
+    StreamRef = gun:post(Pid, Path, Headers, Body),
     gun:close(Pid).
+
